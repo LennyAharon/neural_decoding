861af6e12979d8a9fe11a05c2d866cb4fd4690f9
Lenny Aharon <la2872@columbia.edu>
2026-03-03 09:18:34 -0500

    making plots nice

--- scripts/batch_download_common_data.sh
+++ scripts/batch_download_common_data.sh
@@ -44,9 +44,33 @@ EIDS=(
     # "754b74d5-7a06-4004-ae0c-72a10b6ed2e6"
     # "824cf03d-4012-4ab1-b499-c83a92c5589e"
     # "1735d2be-b388-411a-896a-60b01eaa1cfe" # doesn't have probs data 
-    "3638d102-e8b6-4230-8742-e548cd87a949"
-    "6899a67d-2e53-4215-a52a-c7021b5da5d4"
-    "8928f98a-b411-497e-aa4b-aa752434686d"
+    # "3638d102-e8b6-4230-8742-e548cd87a949"
+    # "6899a67d-2e53-4215-a52a-c7021b5da5d4"
+    # "8928f98a-b411-497e-aa4b-aa752434686d"
+    # # 21 videos for segmentation 
+    # "1735d2be-b388-411a-896a-60b01eaa1cfe", #1
+    # "46794e05-3f6a-4d35-afb3-9165091a5a74" 
+    # "493170a6-fd94-4ee4-884f-cc018c17eeb9" 
+    # "54238fd6-d2d0-4408-b1a9-d19d24fd29ce" 
+    # "73918ae1-e4fd-4c18-b132-00cb555b1ad2" 
+    # "b03fbc44-3d8e-4a6c-8a50-5ea3498568e0" 
+    # "b196a2ad-511b-4e90-ac99-b5a29ad25c22" 
+    # "b22f694e-4a34-4142-ab9d-2556c3487086" 
+    # "c7248e09-8c0d-40f2-9eb4-700a8973d8c8" 
+    # "c7b0e1a3-4d4d-4a76-9339-e73d0ed5425b" # didn't work
+    "c7bf2d49-4937-4597-b307-9f39cb1c7b16" 
+    "ca4ecb4c-4b60-4723-9b9e-2c54a6290a53" 
+    "d0ea3148-948d-4817-94f8-dcaf2342bbbe" 
+    "d23a44ef-1402-4ed7-97f5-47e9a7a504d9" 
+    "db4df448-e449-4a6f-a0e7-288711e7a75a" 
+    "dc962048-89bb-4e6a-96a9-b062a2be1426"
+    "e535fb62-e245-4a48-b119-88ce62a6fe67"
+    "f115196e-8dfe-4d2a-8af3-8206d93c1729"
+    "f140a2ec-fd49-4814-994a-fe3476f14e66"
+    "f3ce3197-d534-4618-bf81-b687555d1883"
+    "ff96bfe1-d925-4553-94b5-bf8297adf259"
+
+    
 
 
 
@@ -54,7 +78,7 @@ EIDS=(
 
 # Pose model path to use for trial filtering (shared across all sessions)
 # This should point to a directory containing pose predictions (e.g., CSV files)
-POSE_MODEL_DIR="test_200_MVT_3d_aug_patch_masking/ensemble_median"
+POSE_MODEL_DIR="test_200_MVT_3d_aug_patch_masking_new/eks_multiview_linear"
 POSE_MODEL_PATH="${POSE_BASE_PATH}/${POSE_MODEL_DIR}/videos_new"
 
 # =============================================================================
